d85564296bda2d87550b033a435bcb78b7c1fed3
Friedrich Paetzke <f.paetzke@gmail.com>
2014-01-11 12:48:26 +0100

    Add more Python versions.

--- setup.py
+++ setup.py
@@ -19,8 +19,11 @@ setup(
         'Intended Audience :: Developers',
         'Operating System :: OS Independent',
         'Programming Language :: Python',
+        'Programming Language :: Python :: 2',
         'Programming Language :: Python :: 2.6',
         'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.3',
         'Topic :: Software Development'
     ],
 )
